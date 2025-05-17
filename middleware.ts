a288bcea11738d52bebad469afaca5f2ef2d9057
wxk <wxkzd-qq.com>
2025-05-17 20:22:17 +0800

    feat: auth

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,67 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  publicRoutes,
+  userRoutes,
+  adminRoutes,
+  premiumRoutes,
+} from "./routes";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const { pathname } = nextUrl;
+  const isLoggedIn = !!req.auth;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isUserRoute = userRoutes.includes(nextUrl.pathname);
+  const isAdminRoute = adminRoutes.includes(nextUrl.pathname);
+  const isPremiumRoute = premiumRoutes.includes(nextUrl.pathname);
+
+  // 根路由重定向
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/browse", nextUrl));
+  }
+  // 允许API认证路由
+  if (isApiAuthRoute) {
+    return;
+  }
+  // 如果未登录且不是公开路由，重定向到登录
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  }
+
+  // 允许认证路由（如登录/注册）
+  // if (isUserRoute) {
+  //     if (isLoggedIn) {
+  //         return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  //     }
+  //     return;
+  // }
+
+  // 角色检查
+  if (isLoggedIn) {
+    const role = req.auth?.user?.role;
+    if (
+      isUserRoute &&
+      role !== "USER" &&
+      role !== "PREMIUM" &&
+      role !== "ADMIN"
+    ) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    // ADMIN路由检查
+    if (isAdminRoute && role !== "ADMIN") {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    // PREMIUM路由检查
+    if (isPremiumRoute && role !== "PREMIUM" && role !== "ADMIN") {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
