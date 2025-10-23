e4f60ad384b5777fa4cdf312165f6699c1aa8251
wang xukun <wang_xukun@163.com>
2025-10-23 22:29:00 +0800

    Bugfix/admin logout (#66)
    Co-authored-by: wxk <wxkzd-qq.com>
    fix: (signup\route.ts) return value #31
    fix: resolves #31

--- components/auth/login-btn.tsx
+++ components/auth/login-btn.tsx
@@ -11,7 +11,7 @@ export default function LoginBtn() {
     return (
       <button
         className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-        onClick={() => signOut({ redirectTo: "/home" })}
+        onClick={() => signOut({ redirectTo: "/" })}
       >
         <PowerIcon className="w-6" />
         <div className="hidden md:block">退出</div>
