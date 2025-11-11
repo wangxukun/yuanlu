3ad3ae2d6caf9f066e4614b627efd906a4bb623e
wang xukun <wang_xukun@163.com>
2025-11-11 15:33:34 +0800

    Merge develop to master (#100)
    Co-authored-by: wxk <wxkzd-qq.com>
    
    * fix: 🐛 修复退出登录重定向URL错误 (#99)

--- components/auth/login-home-btn.tsx
+++ components/auth/login-home-btn.tsx
@@ -3,6 +3,7 @@
 import { useSession, signOut } from "next-auth/react";
 import { UserIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 export default function LoginHomeBtn() {
   const { data: session } = useSession();
 
@@ -11,7 +12,7 @@ export default function LoginHomeBtn() {
       <button
         // className="flex items-center space-x-2 px-4 py-2 hover:drop-shadow-md rounded-lg transition-colors"
         className="btn btn-ghost flex items-center gap-2"
-        onClick={() => signOut({ redirectTo: "/home" })}
+        onClick={() => signOut({ redirectTo: `${baseUrl}/home` })}
       >
         <UserCircleIcon className="w-5 h-5 hidden sm:block" />
         <span className="text-sm font-medium text-gray-700">退出</span>
