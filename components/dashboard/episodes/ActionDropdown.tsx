6bef061098911e461ffaea979cd0c2533921cdd8
wangxk-office <wang_xukun@163.com>
2025-12-08 14:50:57 +0800

    refactor: 💡 update episode delete button form

--- components/dashboard/episodes/ActionDropdown.tsx
+++ components/dashboard/episodes/ActionDropdown.tsx
@@ -65,23 +65,23 @@ const ActionDropdown: React.FC<ActionDropdownProps> = ({ episodeId }) => {
             <span>封面管理</span>
           </button>
           <div className="border-t border-slate-200 my-1"></div>
-          <button className="flex w-full items-center px-4 py-2.5 text-sm text-red-600 hover:bg-red-50 hover:text-red-700 transition-colors">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 mr-3"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-              />
-            </svg>
-            <span>删除稿件</span>
-          </button>
+          {/*<button className="flex w-full items-center px-4 py-2.5 text-sm text-red-600 hover:bg-red-50 hover:text-red-700 transition-colors">*/}
+          {/*  <svg*/}
+          {/*    xmlns="http://www.w3.org/2000/svg"*/}
+          {/*    className="h-4 w-4 mr-3"*/}
+          {/*    fill="none"*/}
+          {/*    viewBox="0 0 24 24"*/}
+          {/*    stroke="currentColor"*/}
+          {/*  >*/}
+          {/*    <path*/}
+          {/*      strokeLinecap="round"*/}
+          {/*      strokeLinejoin="round"*/}
+          {/*      strokeWidth={2}*/}
+          {/*      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"*/}
+          {/*    />*/}
+          {/*  </svg>*/}
+          {/*  <span>删除稿件</span>*/}
+          {/*</button>*/}
           <EpisodeDeleteForm id={episodeId} />
         </div>
       </div>
--- components/dashboard/episodes/EpisodeDeleteForm.tsx
+++ components/dashboard/episodes/EpisodeDeleteForm.tsx
@@ -1,7 +1,11 @@
 import { deleteEpisodeById } from "@/lib/actions";
-import { startTransition, useActionState, useEffect, useState } from "react";
+import React, {
+  startTransition,
+  useActionState,
+  useEffect,
+  useState,
+} from "react";
 import { redirect } from "next/navigation";
-import { TrashIcon } from "@heroicons/react/24/outline";
 import { ActionState } from "@/lib/types";
 
 export function EpisodeDeleteForm({ id }: { id: string }) {
@@ -44,14 +48,33 @@ export function EpisodeDeleteForm({ id }: { id: string }) {
   return (
     <>
       {/* 删除按钮 */}
+      {/*<button*/}
+      {/*    onClick={openModal}*/}
+      {/*    className="flex items-center rounded-md border border-red-500 bg-red-500 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-red-600 focus:outline-hidden focus:ring-2 focus:ring-red-500 focus:ring-offset-2"*/}
+      {/*>*/}
+      {/*  <span className="sr-only">delete</span>*/}
+      {/*  <TrashIcon className="h-5 w-5"/>*/}
+      {/*</button>*/}
       <button
         onClick={openModal}
-        className="flex items-center rounded-md border border-red-500 bg-red-500 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-red-600 focus:outline-hidden focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+        className="flex w-full items-center px-4 py-2.5 text-sm text-red-600 hover:bg-red-50 hover:text-red-700 transition-colors"
       >
-        <span className="sr-only">delete</span>
-        <TrashIcon className="h-5 w-5" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4 mr-3"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+        </svg>
+        <span>删除稿件</span>
       </button>
-
       {/* 自定义模态框 */}
       {isModalOpen && (
         <div
