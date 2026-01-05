4421ce9a870a5151c0ad3a795a443c9edc569fcc
wangxk-office <wang_xukun@163.com>
2026-01-05 09:46:48 +0800

    style: 💄 fix podcast_detail episode descript line-clamp-1

--- components/podcast/PodcastDetail.tsx
+++ components/podcast/PodcastDetail.tsx
@@ -425,15 +425,8 @@ export default function PodcastDetail({
                   currentEpisode?.episodeid === episode.episodeid && !isPlaying;
 
                 return (
-                  // <div
-                  //   key={episode.episodeid}
-                  //   className={`group flex items-center p-3 sm:p-4 rounded-xl border border-transparent hover:border-base-300 hover:bg-base-200/50 transition-all cursor-pointer relative overflow-hidden ${isCurrentPlaying || isCurrentPaused ? "bg-base-200/50 border-primary/20" : "bg-base-100/50 sm:bg-transparent"}`}
-                  //   onClick={() => handleRowClick(episode)}
-                  // >
                   <div
                     key={episode.episodeid}
-                    // [修改 1]: 删除了 "overflow-hidden"
-                    // [修改 2]: 新增了 z-index 逻辑，确保菜单展开时当前行层级最高，不会被下方列表项遮挡
                     className={`group flex items-center p-3 sm:p-4 rounded-xl border border-transparent hover:border-base-300 hover:bg-base-200/50 transition-all cursor-pointer relative ${
                       activeEpisodeMenu === episode.episodeid ? "z-20" : "z-0"
                     } ${
@@ -475,7 +468,7 @@ export default function PodcastDetail({
                     </div>
 
                     {/* Episode Info */}
-                    <div className="ml-3 sm:ml-4 flex-1 min-w-0 flex flex-col justify-center">
+                    <div className="ml-3 sm:ml-4 flex-1 min-w-0 flex flex-col justify-center w-full">
                       {/* [修改重点]:
                           1. 移除 truncate (不再单行截断)
                           2. 添加 break-words (强制换行)
@@ -488,7 +481,7 @@ export default function PodcastDetail({
                         {episode.title}
                       </h3>
 
-                      <p className="hidden sm:block text-sm text-base-content/60 mt-1 mb-1.5 line-clamp-2 leading-relaxed break-words">
+                      <p className="hidden sm:line-clamp-1 text-sm text-base-content/60 mt-1 mb-1.5 line-clamp-2 leading-relaxed break-words">
                         {episode.description || "暂无简介"}
                       </p>
 
